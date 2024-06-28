9c028a774bf074ee9ca39301a0f26adab4f3ffdb
salehabedinezdad@gmail.comgit config --global user.name imsalionegit config --global user.email salehabedinezhad@gmail.com <salehabedinezhad@gmail.com>
2024-06-28 16:55:42 +0330

    new changes

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ document.getElementById('farsiBtn').addEventListener('click', function() {
     document.getElementById('englishText').style.display = 'none';
     document.getElementById('farsiText').style.display = 'block';
 });
+
+document.getElementById('englishBtn').addEventListener('click', function() {
+    document.getElementById('englishTitle').style.display = 'block';
+    document.getElementById('farsiTile').style.display = 'none';
+});
+
+document.getElementById('farsiBtn').addEventListener('click', function() {
+    document.getElementById('englishTitle').style.display = 'none';
+    document.getElementById('farsiTitle').style.display = 'block';
+});
