ddcf269491a511ce60af3c7580718e8a2db6c56f
salehabedinezdad@gmail.comgit config --global user.name imsalionegit config --global user.email salehabedinezhad@gmail.com <salehabedinezhad@gmail.com>
2024-06-28 17:37:16 +0330

    new changes

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 document.getElementById('englishBtn').addEventListener('click', function() {
     document.getElementById('englishText').style.display = 'block';
     document.getElementById('farsiText').style.display = 'none';
+    document.body.style.direction = 'ltr';
 });
 
 document.getElementById('farsiBtn').addEventListener('click', function() {
     document.getElementById('englishText').style.display = 'none';
     document.getElementById('farsiText').style.display = 'block';
+    document.body.style.direction = 'rtl';
 });
 
 document.getElementById('englishBtn').addEventListener('click', function() {
     document.getElementById('englishTitle').style.display = 'block';
-    document.getElementById('farsiTile').style.display = 'none';
+    document.getElementById('farsiTitle').style.display = 'none';
+    document.body.style.direction = 'ltr';
 });
 
 document.getElementById('farsiBtn').addEventListener('click', function() {
     document.getElementById('englishTitle').style.display = 'none';
     document.getElementById('farsiTitle').style.display = 'block';
+    document.body.style.direction = 'rtl';
 });
